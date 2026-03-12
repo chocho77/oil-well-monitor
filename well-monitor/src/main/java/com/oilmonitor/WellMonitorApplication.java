cf0d2caf092d50ec5cc88262eb1f22855f5cc132
chocho77 <chavmom@gmail.com>
2026-03-12 18:09:51 +0200

    feat: Add Spring Boot backend project structure

---
+++ well-monitor/src/main/java/com/oilmonitor/WellMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.oilmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellMonitorApplication.class, args);
+	}
+
+}
---
+++ well-monitor/src/test/java/com/oilmonitor/WellMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oilmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
